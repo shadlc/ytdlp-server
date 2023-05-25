7e9858d352afa74f396858b6e47cb6d9859af3ff
似然可绘 <i@shadlc.net>
2023-05-25 11:27:07 +0800

    Init Commit

---
+++ README.md
@@ -0,0 +1,166 @@
+# [YT-DLP Server](#ytdlp-server)
+
+## Introduction
+YT-DLP Server is an server side for [yt-dlp](https://github.com/yt-dlp/yt-dlp/) that can allow you run it in your server.
+
+## Features
+1. Depend on Flask.
+2. Easy use API, RESTful-API style respond.
+3. Multi-threaded download task.
+4. Download with aria2, each tasks RPC host support.
+5. User-friendly interactive and interface.
+
+## Use
+### Install Aria2
+You need to install aria2 first because this script depends on aria2 for more quickly download speed.
+- https://github.com/aria2/aria2
+### Init Envirment
+    $ python -m venv ytdlp-server
+    $ cd ytdlp-server
+
+* For Windows
+`
+    $ .\Scripts\Activate.ps1
+`
+* For other platform
+`
+    $ source bin/activate
+`
+### Download
+    $ git clone https://github.com/shadlc/ytdlp-server.git
+    $ cd ytdlp-server
+### Install Requirements
+    $ pip install -r requirements.txt
+### Run
+    $ python3 ytdlp-server.py
+
+#### Usage Notes
+When started this script, you can download videos to server via yt-dlp. You can modify the configuration file "config.json". 
+
+## Web Page
+- http://127.0.0.1:8848/
+
+## API
+
+### /download
+Directly download the video via yt-dlp.
+#### params
+- url: The url that input for yt-dlp.
+#### example
+* ##### request
+` GET http://127.0.0.1:8848/download?url=https://www.youtube.com/watch?v=dQw4w9WgXcQ `
+* ##### responed
+```
+{
+  "code": 200,
+  "status": "Added",
+  "data": {
+    "title": "Rick Astley - Never Gonna Give You Up (Official Music Video)",
+    "thumbnail": "https://i.ytimg.com/vi_webp/dQw4w9WgXcQ/maxresdefault.webp",
+    "site": "youtube",
+    "uploader": "Rick Astley",
+    "id": "dQw4w9WgXcQ",
+    "duration": 212,
+    "resolution": "1920x1080",
+    "ext": "webm",
+    "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  }
+}
+```
+### /info
+Get video information via yt-dlp.
+#### params
+- url: The url that input for yt-dlp.
+- type: Default is "brief" and optional in "brief" or "raw".
+#### example
+* ##### request
+` GET http://127.0.0.1:8848/info?url=https://www.youtube.com/watch?v=dQw4w9WgXcQ `
+* ##### responed
+```
+{
+  "code": 200,
+  "status": "Success",
+  "data": {
+    "title": "Rick Astley - Never Gonna Give You Up (Official Music Video)",
+    "thumbnail": "https://i.ytimg.com/vi_webp/dQw4w9WgXcQ/maxresdefault.webp",
+    "site": "youtube",
+    "uploader": "Rick Astley",
+    "id": "dQw4w9WgXcQ",
+    "duration": 212,
+    "resolution": "1920x1080",
+    "ext": "webm",
+    "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "size": 59080956
+  }
+}
+```
+
+### /status
+Get the status about download task.
+#### params
+- None
+#### example
+* ##### request
+` GET http://127.0.0.1:8848/status `
+* ##### responed
+```
+{
+  "code": 200,
+  "status": "Init",
+  "data": {
+    "tasks": {}
+  }
+}
+```
+
+### /history
+Get the history you downloaded.
+#### params
+- None
+#### example
+* ##### request
+` GET http://127.0.0.1:8848/history `
+* ##### responed
+```
+{
+  "code": 400,
+  "status": "No history yet",
+  "data": ""
+}
+```
+
+### /clear_download
+Clear the downloading list.
+#### params
+- None
+#### example
+* ##### request
+` GET http://127.0.0.1:8848/clear_download `
+* ##### responed
+```
+{
+  "code": 400,
+  "status": "cleared",
+  "data": ""
+}
+```
+
+### /clear_history
+Clear the download history.
+#### params
+- None
+#### example
+* ##### request
+` GET http://127.0.0.1:8848/clear_history `
+* ##### responed
+```
+{
+  "code": 400,
+  "status": "cleared",
+  "data": ""
+}
+```
+
+
+## License
+[![License](https://img.shields.io/github/license/shadlc/ytdlp-server.svg?style=flat)](https://github.com/shadlc/ytdlp-server/blob/master/LICENSE)
---
+++ ytdlp-server.py
@@ -0,0 +1,324 @@
+import os
+import time
+import json
+import copy
+import flask
+import socket
+import hashlib
+import requests
+import threading
+from flask_cors import CORS
+from yt_dlp import YoutubeDL
+
+GREEN = '\033[0;32m'
+YELLOW = '\033[0;33m'
+ENDC = '\033[0m'
+app = flask.Flask(__name__)
+CORS(app, resources=r'/*')
+MyYTDLP = None
+
+OPT_FILE = 'config.json'
+LOG_FILE = 'latest.log'
+
+@app.route('/')
+def index():
+  return flask.render_template('index.html')
+
+@app.route('/download')
+def download():
+  global MyYTDLP
+  if flask.request.method == 'POST':
+    url = request.form['url']
+  else:
+    result = flask.request.args
+    url = result['url'] if 'url' in result else ''
+  if url == '':
+    return restful(400, 'No URL input')
+  else:
+    result = MyYTDLP.start(url)
+    if 'Added' == result['status']:
+      return restful(202, result['status'], result['data'])
+    else:
+      return restful(200, result['status'], result['data'])
+
+@app.route('/status')
+def status():
+  global MyYTDLP
+  result = MyYTDLP.task_status()
+  return restful(200, result['status'], result['data'])
+
+@app.route('/info')
+def info():
+  global MyYTDLP
+  if flask.request.method == 'POST':
+    url = request.form['url']
+  else:
+    result = flask.request.args
+    url = result['url'] if 'url' in result else ''
+    mode = result['mode'] if 'mode' in result else ''
+  if url == '':
+    return restful(400, 'No URL input')
+  else:
+    if mode == 'essential':
+      result = MyYTDLP.info(url)
+      return restful(200, result['status'], result['data'])
+    elif mode == 'raw':
+      result = MyYTDLP.info(url,'raw')
+      return restful(200, result['status'], result['data'])
+    else:
+      result = MyYTDLP.info(url)
+      return restful(200, result['status'], result['data'])
+
+@app.route('/history')
+def history():
+  global MyYTDLP
+  if task_list := MyYTDLP.task_history():
+    return restful(200, 'Success', task_list)
+  else:
+    return restful(400, 'No history yet')
+
+@app.route('/clear_download')
+def clear_download():
+  global MyYTDLP
+  MyYTDLP.download_list = {}
+  MyYTDLP.error_code = 0
+  MyYTDLP.status = 'Cleared'
+  return restful(200, MyYTDLP.status)
+
+@app.route('/clear_history')
+def clear_history():
+  global MyYTDLP
+  MyYTDLP.download_history = {}
+  return restful(200, MyYTDLP.status)
+
+def restful(code=200, status='', data=''):
+  response = {
+    'code': code,
+    'status': status,
+    'data': data
+  }
+  return json.dumps(response, indent=2, ensure_ascii=False), code
+
+
+
+class YTDLP():
+  def __init__(self, opts):
+    '''init yt_dlp class'''
+    super(YTDLP, self).__init__()
+    self.download_dir = opts['download_dir']
+    self.opts = opts['ydl']
+    self.outtmpl = self.opts['outtmpl'].copy()
+    self.opts['logger'] = self.Logger()
+    self.opts['paths'] = {'home':self.download_dir}
+    self.download_list = {}
+    self.download_history = {}
+    self.rpc_list = {}
+    self.error_code = 0
+    self.status = 'Init'
+
+  def download_thread(self, url, info):
+    '''download thread'''
+    self.download_list[url] = info
+    self.error_code = self.download(url, info)
+    self.download_list.pop(url)
+    self.download_history[url] = info
+    if self.download_list == {}:
+      self.status = 'Listening'
+    elif self.error_code:
+      self.status = f'Error with code: {self.error_code}'
+    else:
+      self.status = 'Downloading'
+
+  def download(self, url, info):
+    '''yt_dlp download method'''
+    if 'bilibili' in url and os.path.exists("cookies/bilibili.txt"):
+      self.opts['cookiefile'] = "cookies/bilibili.txt"
+    elif 'youtube' in url and os.path.exists("cookies/youtube.txt"):
+      self.opts['cookiefile'] = "cookies/youtube.txt"
+    elif 'qq' in url and os.path.exists("cookies/qqvideo.txt"):
+      self.opts['cookiefile'] = "cookies/qqvideo.txt"
+    opts = self.opts.copy()
+    opts['paths'] = {'home':self.download_dir}
+    opts['outtmpl']['default'] = "[" + info['uploader'] + "] " + self.outtmpl['default']
+    opts['outtmpl']['chapter'] = "[" + info['uploader'] + "] " + self.outtmpl['chapter']
+    # port = find_available_port()
+    # if port:
+    #   md5 = sum_md5(url)
+    #   opts['external_downloader_args'].append(f'--gid={md5[:16]}')
+    #   opts['external_downloader_args'].append(f'--rpc-secret={md5[:16]}')
+    #   opts['external_downloader_args'].append(f'--enable-rpc=true')
+    #   opts['external_downloader_args'].append(f'--rpc-listen-all=true')
+    #   opts['external_downloader_args'].append(f'--rpc-allow-origin-all=true')
+    #   print(f'{YELLOW}Start download for [{url}] with aria2c rpc port at localhost:{port} which GID is {md5[:16]}{ENDC}')
+    #   self.rpc_list[url] = {'port': port, 'token': md5[:16]}
+    # else:
+    #   opts['external_downloader_args'].append('--enable-rpc=false')
+    #   print(f'{YELLOW}Start download for [{url}] with aria2c but has No available port！')
+    ydl = YoutubeDL(opts)
+    ydl.add_progress_hook(self.progress_hook)
+    self.status = 'Downloading'
+    error_code = ydl.download(url)
+    print(f'\n{GREEN}Success download for {url}{ENDC}')
+    return error_code
+
+  def info(self, url, mode='essential'):
+    '''get informations from url'''
+    if 'bilibili' in url and os.path.exists("cookies/bilibili.txt"):
+      self.opts['cookiefile'] = "cookies/bilibili.txt"
+    elif 'youtube' in url and os.path.exists("cookies/youtube.txt"):
+      self.opts['cookiefile'] = "cookies/youtube.txt"
+    elif 'qq' in url and os.path.exists("cookies/qqvideo.txt"):
+      self.opts['cookiefile'] = "cookies/qqvideo.txt"
+    extract_info = YoutubeDL(self.opts).extract_info(url, download=False)
+    if mode == 'raw':
+      return {'status': 'Success', 'data': extract_info}
+    info = {}
+    if 'entries' in extract_info:
+      info['type'] = 'playlist'
+      info['title'] = extract_info.get('title')
+      info['count'] = len(extract_info.get('entries'))
+      info['entires'] = []
+      for i in range(info['count']):
+        entries = extract_info.get('entries')
+        item = {}
+        item['title'] = entries[i].get('title')
+        item['site'] = entries[i].get('extractor')
+        item['thumbnail'] = entries[i].get('thumbnail')
+        item['uploader'] = entries[i].get('uploader')
+        item['id'] = entries[i].get('display_id')
+        item['duration'] = entries[i].get('duration')
+        item['resolution'] = entries[i].get('resolution')
+        item['ext'] = entries[i].get('ext')
+        item['url'] = entries[i].get('webpage_url')
+        item['size'] = entries[i].get('filesize_approx')
+        info['entires'].append(item)
+    else:
+      info['type'] = 'video'
+      info['title'] = extract_info.get('title')
+      info['site'] = extract_info.get('extractor')
+      info['thumbnail'] = extract_info.get('thumbnail')
+      info['uploader'] = extract_info.get('uploader')
+      info['id'] = extract_info.get('display_id')
+      info['duration'] = extract_info.get('duration')
+      info['resolution'] = extract_info.get('resolution')
+      info['ext'] = extract_info.get('ext')
+      info['url'] = extract_info.get('webpage_url')
+      info['size'] = extract_info.get('filesize_approx')
+    if mode == 'essential':
+      return {'status': 'Success', 'data': info}
+    else:
+      return {'status': 'Success', 'data': info}
+    try:
+      pass
+    except:
+      return {'status': 'Invaid URL', 'data': ''}
+
+  def start(self, url):
+    '''detect url and start download'''
+    try:
+      info = self.info(url)['data']
+      url = info['url']
+      if url[-1] == '/':
+        url = url[:-1]
+      if url in self.download_list:
+        return self.task_status()
+      else:
+        threading.Thread(target=self.download_thread, args=(url, info), daemon=True).start()
+        self.status = 'Added'
+        return {'status': self.status, 'data': info}
+    except:
+      return {'status': 'Invaid URL', 'data': ''}
+
+  # def count_process(self):
+  #   for url in self.rpc_list:
+  #     port = self.rpc_list[url]['port']
+  #     token = self.rpc_list[url]['token']
+  #     response = requests.post(f'http://localhost:{port}/jsonrpc', data = '{"jsonrpc":"2.0","method":"aria2.tellActive","id":1,"params": ["token:' + token + '"]}')
+  #     result = json.loads(response.text)
+  #     if not result['result']:
+  #       continue
+  #     result = result['result'][0]
+  #     self.download_list[url]['size'] = result['totalLength']
+  #     self.download_list[url]['download_speed'] = result['downloadSpeed']
+  #     self.download_list[url]['processed'] = result['completedLength']
+
+  def task_status(self):
+    '''return running status and downloading list'''
+    if self.status == 'Added':
+      self.status = 'Downloading'
+    if self.status == 'finished' and self.download_list != {}:
+      self.status = 'Downloading'
+    # self.count_process()
+    return {'status': self.status, 'data': self.download_list}
+
+  def task_history(self):
+    '''downloaded url history'''
+    return self.download_history
+
+  def progress_hook(self, d):
+    '''yt_dlp hook'''
+    self.status = d['status']
+
+  class Logger:
+    '''yt_dlp log class'''
+    def __init__(self):
+      if os.path.exists(LOG_FILE):
+        self.log = open(LOG_FILE, mode='a')
+      else:
+        self.log = open(LOG_FILE, mode='w')
+
+    def __del__(self):
+      self.log.close()
+
+    def debug(self, msg):
+      # For compatibility with youtube-dl, both debug and info are passed into debug
+      # You can distinguish them by the prefix '[debug] '
+      if msg.startswith('[debug] '):
+        pass
+      else:
+        self.info(msg)
+
+    def info(self, msg):
+      prefix = '\n[' + time.strftime('%H:%M:%S', time.localtime()) + ' INFO] '
+      msg = prefix + msg
+      self.log.write(msg)
+
+    def warning(self, msg):
+      prefix = '\n[' + time.strftime('%H:%M:%S', time.localtime()) + ' WARNING] '
+      msg = prefix + msg
+      self.log.write(msg)
+
+    def error(self, msg):
+      prefix = '\n[' + time.strftime('%H:%M:%S', time.localtime()) + ' ERROR] '
+      msg = prefix + msg
+      self.log.write(msg)
+      print(msg)
+
+
+def import_json(file):
+  '''import json'''
+  content = '{}'
+  if not os.path.exists(file):
+    open(file,'w', encoding='utf-8').write('{}')
+  elif temp := open(file, 'r', encoding='utf-8').read():
+    content = temp
+  return json.loads(content)
+
+def sum_md5(string):
+  '''compute the md5'''
+  return hashlib.md5(string.encode('utf-8')).hexdigest()
+
+def find_available_port(interface=''):
+  '''find available port'''
+  try:
+    with socket.socket() as sock:
+      sock.bind((interface, 0))
+      return sock.getsockname()[1]
+  except OSError:
+    return None
+
+if __name__ == '__main__':
+  config = import_json(OPT_FILE)
+  MyYTDLP = YTDLP(config)
+  print('yt_dlp api server start!')
+  app.run(config['host'], config['port'])
